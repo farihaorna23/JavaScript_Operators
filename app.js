7bc9ce99a3aeeaef557113301d3e8991b9ed0393
farihaorna23 <farihaorna@gmail.com>
2022-08-15 13:36:34 -0400

    Finished exercise2

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
+
+// YOUR CODE HERE
 let a = 20;
 let b = 4;
 let add = a + b;
@@ -14,13 +16,36 @@ let multiply = a * b;
 let dividing = a / b;
 
 console.log(add, minus, multiply, dividing);
-// YOUR CODE HERE
-
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 
+//What is the value of: num + str?
+let result1 = num + str;
+console.log(result1); //1111
+//What is the value of: num + str2?
+let result2 = num + str2;
+console.log(result2); //11eleven
+//What is the value of: num + isPresent?
+let result3 = num + isPresent;
+console.log(result3); //12
+//because true value is 1, sp 11 + 1 = 12
+//What is the value of: firstName + num?
+let result4 = firstName + num;
+console.log(result4); //Frodo11
+//What is the value of: isPresent + str?
+let result5 = isPresent + str;
+console.log(result5); //true11
+//What is the value of: firstName + lastName?
+let result6 = firstName + lastName;
+console.log(result6); //FrodoBaggins
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
